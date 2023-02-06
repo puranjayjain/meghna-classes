05b7759d5906a2c53f455e6f9de26d60d9deafb3
Puranjay Jain <puru1joy@gmail.com>
2023-02-06 12:12:30 +0530

    add cards

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Typography from '@mui/joy/Typography';
 
 import Logo from './components/logo/logo';
 import Copyright from './components/copyright/copyright';
+import SubjectCard from './components/subject-card/subject-card';
 
 import './app.scss';
 
@@ -21,6 +22,45 @@ function App() {
             <Button color="warning" component="a" size="sm" href="https://wa.me/+917976382056">
               Contact Now
             </Button>
+          </Grid>
+        </Grid>
+        <Grid pb={2} container spacing={1}>
+          {/* next portion */}
+          <Grid xs={3}>
+            <SubjectCard
+              title="Maths 6th - 8th"
+              subtitle="New Batch Starting Soon"
+              imageSrc="photo-1601397922721-4326ae07bbc5"
+              info="Classes on Alternate days"
+              fees="Rs. 3000 / month"
+            />
+          </Grid>
+          <Grid xs={3}>
+            <SubjectCard
+              title="Maths 9th - 10th"
+              subtitle="New Batch Starting Soon"
+              imageSrc="photo-1560785477-d43d2b34e0df"
+              info="Classes on Alternate days"
+              fees="Rs. 3000 / month"
+            />
+          </Grid>
+          <Grid xs={3}>
+            <SubjectCard
+              title="Science 6th - 8th"
+              subtitle="New Batch Starting Soon"
+              imageSrc="photo-1534643960519-11ad79bc19df"
+              info="Classes on Alternate days"
+              fees="Rs. 3000 / month"
+            />
+          </Grid>
+          <Grid xs={3}>
+            <SubjectCard
+              title="Science 9th - 10th"
+              subtitle="New Batch Starting Soon"
+              imageSrc="photo-1532634993-15f421e42ec0"
+              info="Classes on Alternate days"
+              fees="Rs. 3000 / month"
+            />
           </Grid>
         </Grid>
         <Grid container spacing={2}>
---
+++ src/components/subject-card/subject-card.tsx
@@ -0,0 +1,54 @@
+import AspectRatio from '@mui/joy/AspectRatio';
+import Box from '@mui/joy/Box';
+import Button from '@mui/joy/Button';
+import Card from '@mui/joy/Card';
+import Typography from '@mui/joy/Typography';
+import Alert from '@mui/joy/Alert';
+
+type ComponentProps = {
+  title: string;
+  subtitle: string;
+  info: string;
+  fees: string;
+  imageSrc: string;
+  imgAlt?: string;
+};
+
+export default function Component({ title, subtitle, imageSrc, imgAlt = '', info, fees }: ComponentProps) {
+  const imgSrc = `https://images.unsplash.com/${imageSrc}?auto=format&fit=crop&w=286`;
+  const imgSrcSet = `https://images.unsplash.com/${imageSrc}?auto=format&fit=crop&w=286&dpr=2 2x`;
+
+  return (
+    <Card variant="outlined" sx={{ width: 320 }}>
+      <Typography level="h2" fontSize="md" sx={{ mb: 0.5 }}>
+        {title}
+      </Typography>
+      <Typography level="body2">{subtitle}</Typography>
+      <Alert color="info" size="sm" sx={{ position: 'absolute', top: '0.5rem', right: '0.5rem' }}>
+        CBSE
+      </Alert>
+      <AspectRatio minHeight="120px" maxHeight="200px" sx={{ my: 2 }}>
+        <img src={imgSrc} srcSet={imgSrcSet} loading="lazy" alt={imgAlt} />
+      </AspectRatio>
+      <Box sx={{ display: 'flex' }}>
+        <div>
+          <Typography level="body3">{info}:</Typography>
+          <Typography fontSize="lg" fontWeight="lg">
+            {fees}
+          </Typography>
+        </div>
+        <Button
+          variant="solid"
+          size="sm"
+          color="warning"
+          component="a"
+          href="https://wa.me/+917976382056"
+          aria-label="Explore Bahamas Islands"
+          sx={{ ml: 'auto', fontWeight: 600 }}
+        >
+          Contact
+        </Button>
+      </Box>
+    </Card>
+  );
+}
